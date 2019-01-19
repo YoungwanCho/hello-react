b9216abab8ff823541ce7e4cf9af70dfb8878100
developer119 <developer119korea@gmail.com>
2019-01-19 16:27:09 +0900

    컴포넌트를 만들고 사용해봄

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import MyComponent from './MyComponent';
 
 class App extends Component {
   render() {
+    return (
+      <MyComponent />
+    );
   }
 }
 
