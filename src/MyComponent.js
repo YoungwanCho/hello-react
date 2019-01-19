53eb19cdd764fc551b223a9948a4cbe82f8c0476
developer119 <developer119korea@gmail.com>
2019-01-19 17:17:21 +0900

    컴포넌트 사용
    - 컴포넌트를 만들어봄
    - 내보내고, 불러와 봄
    - props, state를 사용해봄

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,10 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Proptypes from 'prop-types';
 
 class MyComponent extends Component {
+  static defaultProps = {
+    name: '기본이름'
+  }
+  static propTypes = {
+    name: Proptypes.string,
+    age: Proptypes.number.isRequired
+  }
+
+  state = {
+    number: 0
+  }
+
   render() {
     return (
       <div>
-        나의 새롭고 멋진 컴포넌트
+        <p>안녕하세요, 제 이름은 {this.props.name} 입니다.</p>
+        <p>저는 {this.props.age}살 입니다.</p>
+        <p>숫자: {this.state.number}</p>
+        <button onClick={() => {
+          this.setState({
+            number: this.state.number + 1
+          })
+        }
+        }>더하기</button>
       </div>
     )
   }
